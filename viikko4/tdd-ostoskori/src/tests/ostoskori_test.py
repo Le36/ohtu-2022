d0d256f4e99c8a935513f7f414f558010a901f39
Le36 <48809753+Le36@users.noreply.github.com>
2022-03-23 19:44:13 +0200

    test for 2 diff

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -32,3 +32,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 6)
+
+    def test_kaksi_eri(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(Tuote("kk", 6))
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
