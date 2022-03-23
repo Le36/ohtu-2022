c26c9dcdf055e3edf9d3606a57b91a38b02981d2
Le36 <48809753+Le36@users.noreply.github.com>
2022-03-23 20:22:29 +0200

    test for ostokset remove

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -4,26 +4,27 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.saldo = 0
-        self._hinta = 0
         self._ostokset = []
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return self.saldo
+        total = 0
+        for x in self._ostokset:
+            total += x.lukumaara()
+        return total
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
 
     def hinta(self):
-        return self._hinta
+        total = 0
+        for x in self._ostokset:
+            total += x.hinta()
+        return total
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        self.saldo = self.saldo + 1
-        self._hinta = self._hinta + lisattava.hinta()
-
         for x in self._ostokset:
             if x.tuotteen_nimi() == lisattava.nimi():
                 x.muuta_lukumaaraa(1)
@@ -32,8 +33,11 @@ class Ostoskori:
         self._ostokset.append(Ostos(lisattava))
 
     def poista_tuote(self, poistettava: Tuote):
-        for x in self._ostokset:
+        for i, x in enumerate(self._ostokset):
             if x.tuotteen_nimi() == poistettava.nimi():
+                if x.lukumaara() == 1:
+                    del self._ostokset[i]
+                    return
                 x.muuta_lukumaaraa(-1)
                 return
 
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -87,3 +87,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.poista_tuote(maito)
         self.assertEqual(self.kori.ostokset()[0].lukumaara(), 1)
+
+    def test_poisto_tyhjentaa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+        self.assertEqual(len(self.kori.ostokset()), 0)
+        self.assertEqual(self.kori.hinta(), 0)
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
