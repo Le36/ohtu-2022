2730af0b7116461b3193667011c2ceef45a3844e
Le36 <48809753+Le36@users.noreply.github.com>
2022-04-24 18:43:47 +0300

    nhl teht

---
+++ viikko6/query-language/src/QueryBuilder.py
@@ -0,0 +1,21 @@
+import matchers
+
+
+class QueryBuilder:
+    def __init__(self, pino=matchers.All()):
+        self.stack = pino
+
+    def build(self):
+        return self.stack
+
+    def playsIn(self, team):
+        return QueryBuilder(matchers.And(self.stack, matchers.PlaysIn(team)))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(matchers.And(self.stack, matchers.HasAtLeast(value, attr)))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(matchers.And(self.stack, matchers.HasFewerThan(value, attr)))
+
+    def oneOf(self, *queries):
+        return QueryBuilder(matchers.Or(*queries))
